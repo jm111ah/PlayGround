1f4918efe7954cfe56dfbad1b27ae4e6611b103a
tacoyaggi93 <tacoyaggi93@triptopaz.com>
2023-08-02 17:22:12 +0900

    20230802

--- PlayGround/Gecko_Care/Model/Payment.cs
+++ PlayGround/Gecko_Care/Model/Payment.cs
@@ -2,5 +2,9 @@
 {
     public class Payment
     {
+        public int MemberSeq { get; set; }
+        public int TransactionType { get; set; }
+        public DateTime PaymentTime { get; set; }
+        public int Price { get; set; }
     }
 }
---
+++ PlayGround/Gecko_Care/Util/DbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Gecko_Care.Util
+{
+	public class DbContext 
+	{
+		private readonly IConfiguration _config;
+
+		public DbContext(IConfiguration config)
+		{
+			_config = config;
+		}
+
+		public IDbConnection GetConnection(string conn = "GeckoCareDb")
+		{
+			return new SqlConnection(_config.GetConnectionString(conn));
+		}
+	}
+}

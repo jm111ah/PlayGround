0a88a8120b2b8d0300d917a7691ef41aa2255f33
tacoyaggi93 <tacoyaggi93@triptopaz.com>
2023-09-27 08:46:23 +0900

    interface

---
+++ PlayGround/Study_Interface/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace Study_Interface
+{
+    interface ISouce
+    {
+        int GetResult();
+    }
+    public class Study_Interface : ISouce
+    {
+        public int GetResult() { return 10; }
+    }
+}
---
+++ PlayGround/Sutdy_dd/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+Study_Interface st = new Study_Interface();
+
+
+st.test();
+
+
+interface ISouce
+{
+    int GetResult();
+}
+public class Study_Interface : ISouce
+{
+    public int GetResult() { return 10; }
+
+    public void test()
+    {
+        Target target = new Target();
+        target.Do(this);
+    }    
+}
+
+class Target
+{
+    public void Do(ISouce obj)
+    {
+        Console.WriteLine(obj.GetResult()); 
+    }
+}
